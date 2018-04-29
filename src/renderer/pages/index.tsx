826a631a99de717b30cc5ecb6270893e6f348a21
Hunter hodnett <hunterhodnett@Hunters-MacBook-Pro.local>
2018-04-28 17:57:26 -0700

    Update dependencies

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Editor from './editor';
 import Login from './login';
@@ -8,17 +8,6 @@ import Login from './login';
 const AppRouter = (
   <Router>
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-
-      <hr />
-
       <Route exact path="/" component={ Editor } />
       <Route path="/login" component={ Login } />
     </div>
