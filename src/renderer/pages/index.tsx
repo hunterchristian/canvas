d6222f1c23f204bc24606d5e545c10dcace6d92f
Hunter hodnett <hunterhodnett@Hunters-MacBook-Pro.local>
2018-07-02 20:06:33 -0700

    Fix rendering of Editor in production distribution build

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MemoryRouter as Router, Route } from 'react-router-dom';
 
 import Editor from './Editor';
 import Login from './Login';
